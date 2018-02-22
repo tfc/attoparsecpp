2ceed6096b7862c2848b5d591a5f6b988798318f
Jacek Galowicz <jacek@galowicz.de>
2018-02-22 19:53:15 +0100

    Make template types a bit better to read

--- include/parser.hpp
+++ include/parser.hpp
@@ -43,11 +43,11 @@ static std::ostream& operator<<(std::ostream& os, const str_pos &p)
 template <typename T>
 using parser = std::optional<std::pair<T, str_pos>>;
 
-template <typename P>
-using parser_ret = decltype(std::declval<P>()(str_pos::from_str("")));
+template <typename Parser>
+using parser_ret = decltype(std::declval<Parser>()(str_pos::from_str("")));
 
-template <typename P>
-using parser_payload_type = decltype(std::declval<P>()(str_pos::from_str(""))->first);
+template <typename Parser>
+using parser_payload_type = decltype(std::declval<Parser>()(str_pos::from_str(""))->first);
 
 template <typename F>
 static auto not_at_end(F f)
@@ -86,8 +86,8 @@ static auto oneOf(Cs ... cs) {
     return sat([cs...] (char c) { return ((c == cs) || ...); });
 }
 
-template <typename P>
-static auto many(P p, bool minimum_one = false) {
+template <typename Parser>
+static auto many(Parser p, bool minimum_one = false) {
     return [p, minimum_one] (str_pos pos) -> parser<std::string> {
         std::string ss;
         while (auto ret {p(pos)}) {
@@ -99,14 +99,14 @@ static auto many(P p, bool minimum_one = false) {
     };
 }
 
-template <typename P>
-static auto many1(P p) {
+template <typename Parser>
+static auto many1(Parser p) {
     return many(p, true);
 }
 
-template <typename P>
-static auto manyV(P p, size_t reserve_items = 0) {
-    using T = parser_payload_type<P>;
+template <typename Parser>
+static auto manyV(Parser p, size_t reserve_items = 0) {
+    using T = parser_payload_type<Parser>;
     return [p, reserve_items] (str_pos pos) -> parser<std::vector<T>> {
         std::vector<T> v;
         v.reserve(reserve_items);
@@ -132,8 +132,8 @@ static parser<int> integer(str_pos p) {
     return {{accum, cursor}};
 }
 
-template <typename P>
-static auto token(P parser) {
+template <typename Parser>
+static auto token(Parser parser) {
     return not_at_end([parser] (str_pos p) -> decltype(parser(str_pos::from_str(""))) {
         if (auto ret {parser(p)}) {
             auto [c, newpos] = *ret;
@@ -145,10 +145,10 @@ static auto token(P parser) {
     });
 }
 
-template <typename P1, typename P2>
-static auto chainl1(P1 item_parser, P2 op_parser)
+template <typename Parser1, typename Parser2>
+static auto chainl1(Parser1 item_parser, Parser2 op_parser)
 {
-    using T = parser_payload_type<P1>;
+    using T = parser_payload_type<Parser1>;
     return [item_parser, op_parser] (str_pos p) -> parser<T> {
         auto i {item_parser(p)};
         if (!i) { return {}; }
@@ -171,10 +171,10 @@ static auto chainl1(P1 item_parser, P2 op_parser)
     };
 }
 
-template <typename P1, typename P2>
-static auto prefixed(P1 prefix_parser, P2 parser)
+template <typename Parser1, typename Parser2>
+static auto prefixed(Parser1 prefix_parser, Parser2 parser)
 {
-    return [prefix_parser, parser] (str_pos pos) -> parser_ret<P2> {
+    return [prefix_parser, parser] (str_pos pos) -> parser_ret<Parser2> {
         if (auto ret1 {prefix_parser(pos)}) {
             return parser(ret1->second);
         }
@@ -182,10 +182,10 @@ static auto prefixed(P1 prefix_parser, P2 parser)
     };
 }
 
-template <typename P1, typename P2>
-static auto postfixed(P1 suffix_parser, P2 parser)
+template <typename Parser1, typename Parser2>
+static auto postfixed(Parser1 suffix_parser, Parser2 parser)
 {
-    return [suffix_parser, parser] (str_pos pos) -> parser_ret<P2> {
+    return [suffix_parser, parser] (str_pos pos) -> parser_ret<Parser2> {
         if (auto ret1 {parser(pos)}) {
             if (auto ret2 {suffix_parser(ret1->second)}) {
                 return {{ret1->first, ret2->second}};
@@ -195,14 +195,14 @@ static auto postfixed(P1 suffix_parser, P2 parser)
     };
 }
 
-template <typename P1, typename P2, typename P3>
-static auto clasped(P1 open_parser, P2 close_parser, P3 parser)
+template <typename Parser1, typename Parser2, typename Parser3>
+static auto clasped(Parser1 open_parser, Parser2 close_parser, Parser3 parser)
 {
     return postfixed(close_parser, prefixed(open_parser, parser));
 }
 
-template <typename P1, typename P2>
-static auto choice(P1 p1, P2 p2)
+template <typename Parser1, typename Parser2>
+static auto choice(Parser1 p1, Parser2 p2)
 {
     return [p1, p2] (str_pos pos) {
         if (auto ret1 {p1(pos)}) {
@@ -212,14 +212,14 @@ static auto choice(P1 p1, P2 p2)
     };
 }
 
-template <typename P>
-static auto run_parser(P &&p, const std::string &s)
+template <typename Parser>
+static auto run_parser(Parser &&p, const std::string &s)
 {
     return p(str_pos::from_str(s));
 }
 
-template <typename P>
-static std::optional<parser_payload_type<P>> parse_result(P &&p, const std::string &s)
+template <typename Parser>
+static std::optional<parser_payload_type<Parser>> parse_result(Parser &&p, const std::string &s)
 {
     if (auto ret {run_parser(p, s)}) {
         return {ret->first};
