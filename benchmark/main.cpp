06faf56df83fcaa15873f056297880bd5534e314
Jacek Galowicz <jacek@galowicz.de>
2018-02-24 17:27:50 +0100

    Add csv_line benchmark

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -44,6 +44,29 @@ BENCHMARK("vector<int> of   100 items", [](benchpress::context* ctx) { measure_v
 BENCHMARK("vector<int> of  1000 items", [](benchpress::context* ctx) { measure_vector_filling(1000, ctx); })
 BENCHMARK("vector<int> of 10000 items", [](benchpress::context* ctx) { measure_vector_filling(10000, ctx); })
 
+static auto csv_line(size_t reserve_items = 0) {
+    return [reserve_items] (str_pos pos) {
+        const auto comma_whitespace {prefixed(oneOf(','), many(oneOf(' ')))};
+        return sep_by(integer, comma_whitespace, reserve_items)(pos);
+    };
+}
+
+static void csv_vector_of_ints(size_t size, benchpress::context* ctx) {
+    assert(size > 0);
+    const std::string s {std::string{"1"} + self_concat(", 1", size - 1)};
+    const auto p {csv_line(size)};
+
+    for (size_t i = 0; i < ctx->num_iterations(); ++i) {
+        const auto r {parse_result(p, s)};
+        assert(r->size() == size);
+    }
+}
+
+BENCHMARK("csv vector of    10 ints", [](benchpress::context* ctx) { csv_vector_of_ints(10, ctx); })
+BENCHMARK("csv vector of   100 ints", [](benchpress::context* ctx) { csv_vector_of_ints(100, ctx); })
+BENCHMARK("csv vector of  1000 ints", [](benchpress::context* ctx) { csv_vector_of_ints(1000, ctx); })
+BENCHMARK("csv vector of 10000 ints", [](benchpress::context* ctx) { csv_vector_of_ints(10000, ctx); })
+
 static void sum_of_ints(size_t size, benchpress::context* ctx) {
     assert(size > 0);
     const std::string s {std::string{"1 "} + self_concat("+ 1", size - 1)};
