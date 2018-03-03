a1a77d87e3bbbf69a6e77f72f532f8dcf84527e8
Jacek Galowicz <jacek@galowicz.de>
2018-03-03 16:32:47 +0100

    make default.nix capable of building attoparsec headers as a package

--- default.nix
+++ default.nix
@@ -5,7 +5,19 @@
 rec {
   myProject = stdenv.mkDerivation {
     name = "attoparsecpp";
-    version = "dev-0.1";
+    version = "0.1";
+
+    src = ./.;
+    checkPhase = ''
+      make -C test check
+      make -C benchmark check
+    '';
+
+    installPhase = ''
+      mkdir -p $out/include
+      cp -r include/attoparsecpp $out/include/
+    '';
+
     buildInputs = with pkgs; [
       (callPackage ./catch.nix { stdenv = stdenv; })
       (callPackage ./googlebench.nix { stdenv = stdenv; })
