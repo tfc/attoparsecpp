27f70de641f985ce8b6fc788c9a993bd51b7d156
Jacek Galowicz <jacek@galowicz.de>
2020-03-23 17:26:06 +0100

    Fix default.nix

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
 }:
 let
   myStdenv = with pkgs; overrideCC stdenv gcc9;
-in myStdenv.mkDerivation {
+in pkgs.stdenv.mkDerivation {
   name = "attoparsecpp";
   buildInputs = with pkgs; [ catch2 gbenchmark ];
   nativeBuildInputs = with pkgs; [ cmake ];
